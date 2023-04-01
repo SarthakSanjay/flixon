02790dfdc00e9e37e145b8514180172b9ae67acd
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-04-02 01:18:39 +0530

    ic

--- scripts/home.js
+++ scripts/home.js
@@ -2,3 +2,4 @@ document.getElementById("notch").addEventListener("click" , function(){
     document.getElementById("sidebar").classList.toggle("open")
     // document.getElementById("btn").style.content.rotate = "-90deg"
 })
+
